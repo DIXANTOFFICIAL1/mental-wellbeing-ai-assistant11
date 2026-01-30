ca3975a3778eb5c60b3a7acffa3abe6f35ae6320
DIXANT SONI <dixantofficial@gmail.com>
2026-01-30 21:46:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from services.ai_service import analyze_wellbeing
 
 app = Flask(__name__)
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -11,9 +10,9 @@ def index():
 
         result = analyze_wellbeing(mood, text)
         return render_template("result.html", result=result)
-
     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
